f47b7e01d8483d252cd825ecca1111d1bd08d739
Herman Adu <herman.adu@hotmail.com>
2025-05-30 16:30:42 +0100

    implmented top and bottoms sections if the chaat compponent,and react emoji picker in the footer

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,84 @@
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  return <div className="chat">Chat Component</div>;
+  // Emoji Module
+  const [open, setOpen] = useState(false);
+
+  // update the input with text
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  /* const handleEmoji = (e) => {
+    //console.log("Emoji Events: ", e);
+
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  }; */
+
+  // check values after handleEmoji is run
+  //console.log("Input Text: ", text);
+
+  return (
+    <div className="chat">
+      {/* Top Section Chat List */}
+
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+
+          <div className="texts">
+            <span>Herman Adu</span>
+            <p>Lorem ipsum dolor, sit amet.</p>
+          </div>
+        </div>
+
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+
+      {/*  Center Section Chats */}
+      <div className="center">Center section</div>
+
+      {/* Bottom Section */}
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
