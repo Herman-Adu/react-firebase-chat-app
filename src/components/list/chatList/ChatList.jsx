ed6c8c48f540ada5ab18f9cffc5c35ea5deed594
Herman Adu <herman.adu@hotmail.com>
2025-05-30 01:53:41 +0100

    added user info and search bar design to chat list

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./chatList.css";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div>
-      <span>Chat List</span>
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
     </div>
   );
 };
--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -2,8 +2,16 @@ import "./userInfo.css";
 
 const Userinfo = () => {
   return (
-    <div>
-      <span>User info</span>
+    <div className="userInfo">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>John Doe</h2>
+      </div>
+      <div className="icons">
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+      </div>
     </div>
   );
 };
